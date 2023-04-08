407f95a0f97412a32838bd434dbeb7aa8570915a
Biswajeet Raut <85820671+BiswajeetRaut@users.noreply.github.com>
2023-04-08 20:42:06 +0530

    Add files via upload

---
+++ build/precache-manifest.9f3e5d457b6881db523582a8c1b51f8e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9346d13b4476d9f00a9f1888eac055cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fe1c5f5767e094d1780",
+    "url": "/static/css/main.10541f89.chunk.css"
+  },
+  {
+    "revision": "42170519b4d793d7f205",
+    "url": "/static/js/2.eda769ca.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.eda769ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe1c5f5767e094d1780",
+    "url": "/static/js/main.794a9589.chunk.js"
+  },
+  {
+    "revision": "8f059f6862843e6ded99",
+    "url": "/static/js/runtime-main.683ead93.js"
+  },
+  {
+    "revision": "bad82695b77d7812a9a09224321ca827",
+    "url": "/static/media/Group_22.bad82695.svg"
+  },
+  {
+    "revision": "f96f8ea1892ccb1955b3b6cc4a3e5246",
+    "url": "/static/media/available_organs.f96f8ea1.svg"
+  },
+  {
+    "revision": "542addca464fe906eb12a3f4f193a715",
+    "url": "/static/media/cover-oasis.542addca.png"
+  },
+  {
+    "revision": "f23bf0a8566b18c8569d11f9010dc13f",
+    "url": "/static/media/donate_an_organ.f23bf0a8.svg"
+  },
+  {
+    "revision": "5876cebbfe7852e001a45dd9cb2dead0",
+    "url": "/static/media/login.5876cebb.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "37e6d73a6df68e1afd8f196a7fc21ffa",
+    "url": "/static/media/policy_compliance.37e6d73a.svg"
+  },
+  {
+    "revision": "e23e60623cbd3decf42ae8df6232587f",
+    "url": "/static/media/request_an_organ.e23e6062.svg"
+  }
+]);
